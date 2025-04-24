eb627cc7c88e3884d9afbfcd020c1d340d3e5a1f
Gaurav Parker <gauravparker955@gmail.com>
2025-04-24 18:03:49 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbaar from './components/header/navbaar';
+
+function App() {
+  return (
+    <>
+    <Navbaar />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/header/navbaar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./navbaar.css"
+
+
+function Navbaar() {
+  return (
+    <header>
+      <nav>
+        <div classname = "left">
+          <img src="./amazon_PNG25.png" alt=""/>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbaar
